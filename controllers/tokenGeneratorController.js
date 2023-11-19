0ce60313946c3bf1f78f1cddf3234df22f0c1a9a
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-11-19 21:38:00 +0600

    tested jwt for google

--- controllers/tokenGeneratorController.js
+++ controllers/tokenGeneratorController.js
@@ -2,12 +2,7 @@ const JWT = require("jsonwebtoken");
 
 const tokenGeneratorController = (req, res) => {
   const { uid, email, role } = req.body;
-  console.log(
-    "✨ 🌟  tokenGeneratorController  uid, email, role:",
-    uid,
-    email,
-    role
-  );
+
   const accessToken = JWT.sign({ email: email }, process.env.SECRET_KEY, {
     expiresIn: "1m",
   });
