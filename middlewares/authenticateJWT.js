25358e7d2c8f3337c3df42230991e0a5cbecc4df
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-11-21 18:47:03 +0600

    bearer token code set

--- middlewares/authenticateJWT.js
+++ middlewares/authenticateJWT.js
@@ -2,14 +2,16 @@ const JWT = require("jsonwebtoken");
 
 const authenticateJWT = (req, res, next) => {
   try {
-    /* const headersToken = req.headers.Authorization || req.headers.authorization;
+    const headersToken = req.headers.Authorization || req.headers.authorization;
     const token = headersToken?.split(" ")[1];
     console.log(
       "🚀 ~ file: authenticateJWT.js:7 ~ authenticateJWT ~ token:",
       token
-    ); */
-    const token = req.cookies.access_token;
-    console.log(token);
+    );
+
+    // for cookies
+    // const token = req.cookies.access_token;
+
     JWT.verify(token, process.env.SECRET_KEY, (err, user) => {
       if (err) return res.status(403).json("token is not valid");
       req.user = user;
