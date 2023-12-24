a03728233fe5e4ae8b0c35894d0ee3f51ce54cfe
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2023-12-24 10:11:13 +0600

    changed auth.js

--- middlewares/authenticateJWT.js
+++ middlewares/authenticateJWT.js
@@ -1,7 +1,12 @@
 const JWT = require("jsonwebtoken");
 
-const authenticateJWT = (req, res, next) => {
-  const { refreshTokenExpiry } = req?.body;
+const authenticateJWT = async (req, res, next) => {
+  const { refreshTokenExpiry } = req.body;
+
+  console.log(
+    "🚀 ~ file: authenticateJWT.js:5 ~ authenticateJWT ~ refreshTokenExpiry:",
+    refreshTokenExpiry
+  );
   try {
     // for localStorage bearer token
     const headersToken = req.headers.Authorization || req.headers.authorization;
@@ -11,20 +16,20 @@ const authenticateJWT = (req, res, next) => {
     JWT.verify(token, process.env.SECRET_KEY, async (err, user) => {
       if (err) {
         console.log("AUTHENTICATEjWT -LINE-12", err.name, err.message);
-        return await res.status(403).json({
+        await res.status(403).json({
           msg: err.message,
           name: err.name,
           srvFile: "authenticateJWT.js",
-          refreshTokenExpiry: refreshTokenExpiry,
+          refreshTokenExpiry,
         });
+      } else {
+        req.user = user;
+        next();
       }
-
-      req.user = user;
-      next();
     });
   } catch (error) {
     console.log("✨ 🌟  authenticateJWT  error line23 ❌❌❌", error, "❌❌❌");
-    res.status(500);
+    await res.status(500);
   }
 };
 module.exports = authenticateJWT;
