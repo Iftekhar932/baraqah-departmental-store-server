476644fffe98985cff8cb3d3d3e3a71488253ca5
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2024-01-17 15:13:37 +0600

    AGAIN CHANGED

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -3,7 +3,7 @@
 const admin = require("firebase-admin");
 // const serviceAccount = require("./etc/secrets/baraqah-departmental-store-firebase-adminsdk-zpj3w-661b287694");
 // const serviceAccount = require("./baraqah-departmental-store-firebase-adminsdk-zpj3w-661b287694");
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
+const serviceAccount = process.env.FIREBASE_ADMIN_CREDENTIALS;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
