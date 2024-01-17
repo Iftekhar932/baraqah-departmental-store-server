7e72cee532fd04a6dfdb051c049cbb8740dbdebe
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2024-01-17 15:08:13 +0600

    changed path

--- firebase/firebaseAdmin.js
+++ firebase/firebaseAdmin.js
@@ -1,7 +1,9 @@
 // firebaseAdmin.js
 
 const admin = require("firebase-admin");
-const serviceAccount = require("./baraqah-departmental-store-firebase-adminsdk-zpj3w-661b287694");
+const serviceAccount = require("./etc/secrets/baraqah-departmental-store-firebase-adminsdk-zpj3w-661b287694");
+// const serviceAccount = require("./baraqah-departmental-store-firebase-adminsdk-zpj3w-661b287694");
+// const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
