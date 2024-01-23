0fbf8323fb0ce3a3a070c8f249305897fe044e8b
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2024-01-23 15:01:53 +0600

    changed cors in index.js

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const corsOptions = require("./config/corsOptions.js");
 
 // middlewares
 app.use(cors(corsOptions));
+app.options("*", cors());
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+app.use(allowCrossDomain);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
