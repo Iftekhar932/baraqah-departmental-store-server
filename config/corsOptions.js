f9593ce6b0814b92612f668b9d9d9d8120893bbc
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2024-01-29 12:01:57 +0600

    corsoptions new callback added

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,4 +1,4 @@
-const whitelist = [
+/* const whitelist = [
   "http://127.0.0.1:5500",
   "http://localhost:3001",
   "https://baraqah-departmental-store-server.onrender.com",
@@ -11,6 +11,30 @@ const corsOptions = {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+module.exports = corsOptions;
+ */
+
+// if this one works above one is not needed
+const whitelist = [
+  "http://localhost:3001",
+  "http://127.0.0.1:5500",
+  "https://baraqah-departmental-store-server.onrender.com",
+  "http://localhost:3000",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(null, true); // Allow any origin
+      // callback(new Error("Not allowed by CORS"));
     }
   },
   optionsSuccessStatus: 200,
