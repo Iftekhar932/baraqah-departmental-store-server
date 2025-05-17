8a8f9af0f446445689fbcc4f8c203d348d798c73
Iftekhar Bin Aziz <iftekharaziz932@gmail.com>
2025-05-17 22:17:34 +0600

    improved userSchema with validator function

--- Schemas/UserSchema.js
+++ Schemas/UserSchema.js
@@ -7,10 +7,23 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid email address"], // Email validation
   },
   password: {
     type: String,
-    require: true,
+    required: true,
+    minlength: [8, "Password must be at least 8 characters long"],
+    validate: {
+      validator: function (value) {
+        // Regex for strong password: at least 1 uppercase, 1 lowercase, 1 number, 1 special character
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+          value
+        );
+      },
+      message:
+        "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+    },
   },
   img: {
     type: String,
